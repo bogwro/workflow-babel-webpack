160a9431b3047ff6752bc1925e271ed7646c4b3b
Bogumil Wrona <b.wrona@cre8newmedia.com>
2016-02-19 22:30:37 +0100

    feat: make host and port configurable
    configure it in one place or pass it as argument in terminal

--- config/globals.js
+++ config/globals.js
@@ -1,9 +1,12 @@
+/*global argv*/
+
 'use strict';
 
 var path = require('path');
 
 global.root = path.join(__dirname, '..');
 
+global.argv = require('minimist')(process.argv.slice(2));
 global.gulp = require('gulp');
 global.WebpackDevServer = require('webpack-dev-server');
 global.webpack = require('webpack');
@@ -11,5 +14,8 @@ global.fs = require('fs');
 global.path = path;
 global.del = require('del');
 global.runSequence = require('run-sequence');
+
+global.PORT = argv.port || argv.p || process.env['npm_package_config_port'] || 8080;
+global.HOST = argv.host || argv.h || process.env['npm_package_config_host'] || 'localhost';
 
 global.config = require('./config');
--- config/gulp/tasks/webpack-dev-server.js
+++ config/gulp/tasks/webpack-dev-server.js
@@ -1,3 +1,5 @@
+/*global config HOST PORT webpack WebpackDevServer*/
+
 'use strict';
 
 require('../../globals');
@@ -7,24 +9,37 @@ var open = require('open');
 exports.task = function(callback) {
   var devConfig = Object.create(config.webpackConfig);
   var compiler;
+  var host = HOST || 'localhost';
+  var port = PORT || 8080;
 
-  devConfig.entry.bundle.unshift('webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080');
+  Object.keys(devConfig.entry).forEach(function(key) {
+    devConfig.entry[key].unshift('webpack/hot/dev-server', 'webpack-dev-server/client?http://' + host + ':' + port);
+  });
 
   devConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  devConfig.devtool = 'eval-source-map';
+  devConfig.debug = true;
+  devConfig.progress = true;
+
+  devConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  devConfig.plugins.push(new webpack.SourceMapDevToolPlugin({}));
 
   compiler = webpack(devConfig);
 
   new WebpackDevServer(compiler, {
-    contentBase: 'build'
-  }).listen(8080, 'localhost', function(error) {
+    contentBase: devConfig.devServer.contentBase || 'build',
+    host,
+    hot: true
+  }).listen(port, host, function(error) {
     if(error) {
-      console.log('ERROR', error.message);
+      console.log('ERROR', error.message); // eslint-disable-line
     }
 
     callback(error);
 
     setTimeout(function () {
-      open('http://localhost:8080');
+      open('http://' + host + ':' + port);
     }, 1000);
   });
 
