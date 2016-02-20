44dd91df88457df12dc751e49b937fe1d9873a26
Bogumil Wrona <bogumil.wrona@gmail.com>
2016-02-20 14:08:13 +0100

    Update webpack-build-prod.js

--- config/gulp/tasks/webpack-build-prod.js
+++ config/gulp/tasks/webpack-build-prod.js
@@ -12,9 +12,7 @@ exports.task = function(callback) {
 
   prodConfig.output.path = path.join(root, 'dist');
 
-  prodConfig.plugins.push([
-    new webpack.optimize.UglifyJsPlugin()
-  ]);
+  prodConfig.plugins.push(new webpack.optimize.UglifyJsPlugin());
 
   webpack(prodConfig, function(err, stats) {
     if(err) {
